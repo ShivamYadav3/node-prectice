efa3ec21e533d8865072363875d71acd9647cd5d
Shivam Yadav <shivamyadav93000@gmail.com>
2023-10-17 22:57:32 +0530

    api

---
+++ day-1/V-11/data.js
@@ -0,0 +1,6 @@
+const data = [
+  { name: "shivam", email: "shivamyadav9300@gmail.com" },
+  { name: "shivam", email: "shivamyadav9300@gmail.com" },
+  { name: "shivam", email: "shivamyadav9300@gmail.com" },
+];
+module.exports = data;
---
+++ day-1/V-6/index.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+fs.writeFileSync("text.txt", "hello shivam");
+console.log(__dirname);
+console.log(__filename);

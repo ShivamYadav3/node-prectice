281042708a4919384f681003653e5d244b882e88
Shivam Yadav <shivamyadav93000@gmail.com>
2023-10-19 00:55:08 +0530

    v-25

--- day-1/V-20/index.js
+++ day-1/V-20/index.js
@@ -9,6 +9,51 @@ const publicPath = path.join(__dirname, "public");
 
 // access without extension
 
+// app.get("/", (req, res) => {
+//   res.sendFile(`${publicPath}/index.html`);
+// });
+// app.get("/about", (req, res) => {
+//   res.sendFile(`${publicPath}/about.html`);
+// });
+// app.get("/home", (req, res) => {
+//   res.sendFile(`${publicPath}/home.html`);
+// });
+// app.get("*", (req, res) => {
+//   res.sendFile(`${publicPath}/pageNotFound.html`);
+// });
+
+// v-24
+//Ejs template engine used for dynamic pages
+
+// app.set("view engine", "ejs");
+
+// app.get("/profile", (req, res) => {
+//   const user = {
+//     name: "shivam",
+//     batch: "c8",
+//     skills: ["html", "css", "js", "react", "redux"],
+//   };
+//   res.render("profile", { user });
+// });
+
+// app.get("/login", (req, res) => {
+//   res.render("login");
+// });
+
+// v-25 middle ware for routing
+
+const reqFilter = (req, res, next) => {
+  if (!req.query.age) {
+    res.send("Please provide age");
+  } else if (req.query.age < 18) {
+    res.send("You can not access this page");
+  } else {
+    next();
+  }
+};
+
+app.use(reqFilter);
+
 app.get("/", (req, res) => {
   res.sendFile(`${publicPath}/index.html`);
 });
@@ -17,9 +62,6 @@ app.get("/about", (req, res) => {
 });
 app.get("/home", (req, res) => {
   res.sendFile(`${publicPath}/home.html`);
-});
-app.get("*", (req, res) => {
-  res.sendFile(`${publicPath}/pageNotFound.html`);
 });
 
 app.listen(5000);
